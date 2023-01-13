2995a85d65bb8adcaabef1cffe4d25c1b4ceeb57
therreth <thenry2@aceproject.space>
2023-01-13 08:53:51 -0600

    68. Module Summary

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,17 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
+  const router = useRouter();
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "Million" },
+    });
+  }
   return (
     <div>
       <h1>The Projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
